a37b2873acaea62f4f761018ec7853d401e00361
unozerocode <enrique@unozerocode.com>
2020-04-30 16:19:27 -0500

    Run generator

--- lib/chirp_web.ex
+++ lib/chirp_web.ex
@@ -82,6 +82,7 @@ defmodule ChirpWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import ChirpWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
