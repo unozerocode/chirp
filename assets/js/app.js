8d2f70956b5ade7a7ef25a051eb0367738088f91
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-08-20 13:35:45 -0500

    fonts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.scss";
+
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,6 +13,7 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
+
 import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
